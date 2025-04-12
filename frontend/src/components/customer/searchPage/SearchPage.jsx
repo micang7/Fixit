4bd52fe553234ae0024acab4b65a0da9dcf1fc18
micang7 <micang7>
2025-04-12 14:46:26 +0200

    Add frontend structure
    Add jsx and css files

---
+++ frontend/src/components/customer/searchPage/SearchPage.jsx
@@ -0,0 +1,7 @@
+import styles from "./SearchPage.module.css";
+
+function SearchPage() {
+  return <h1>SearchPage</h1>;
+}
+
+export default SearchPage;
---
+++ frontend/src/components/repairman/loginPage/LoginPage.jsx
@@ -0,0 +1,7 @@
+import styles from "./LoginPage.module.css";
+
+function LoginPage() {
+  return <h1>LoginPage</h1>;
+}
+
+export default LoginPage;
