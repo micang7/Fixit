ce5117abebe8213825e8fa429510bfe7c91c77fd
Felipe <fkonfederak.fk@gmail.com>
2025-04-22 21:47:20 +0200

    Designed navbar, login page and register page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,29 @@
 import styles from "./App.module.css";
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <nav className={styles.navbar}>
-        <h1 className={styles.h1}>Fixit</h1>
-        <p1 className={styles.moj_styl}>Login</p1>
-        <p1>Register</p1>
+      <nav className={styles.top}>
+        <div className={styles.left}>
+          <Link to="/" className={styles.link}>
+            <img src="/logo.png" alt="logo" className={styles.logo} />
+          </Link>
+        </div>
+        <div className={styles.top}>
+          <p className={styles.contactItem}>📱+48 111 111 111</p>
+          <p className={styles.contactItem}>📩fixit@gmail.com</p>
+        </div>
+        <div className={styles.right}>
+          <div className={styles.accountLinks}>
+            <Link to="/customer-request" className={styles.link}>
+              📄 Request status
+            </Link>
+            <Link to="/repairman-login" className={styles.link}>
+              🧰 Repairman zone
+            </Link>
+          </div>
+        </div>
       </nav>
       <Outlet />
     </>
--- frontend/src/components/repairman/registerPage/RegisterPage.jsx
+++ frontend/src/components/repairman/registerPage/RegisterPage.jsx
@@ -1,7 +1,34 @@
 import styles from "./RegisterPage.module.css";
+import { Link } from "react-router-dom";
 
 function RegisterPage() {
-  return <h1>RegisterPage</h1>;
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.title}>Register</h2>
+      <form className={styles.form}>
+        <label>
+          Email:
+          <input type="email" />
+        </label>
+
+        <label>
+          Password:
+          <input type="password" />
+        </label>
+
+        <label>
+          Confirm password:
+          <input type="password" />
+        </label>
+
+        <button type="submit">Register</button>
+      </form>
+
+      <p className={styles.loginLink}>
+        <Link to="/repairman-login">Log in here</Link>
+      </p>
+    </div>
+  );
 }
 
 export default RegisterPage;
