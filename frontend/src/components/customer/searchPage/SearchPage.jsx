565282a130ec9dd130b2af18d292a4d77345e101
Kuchy777 <Kuchy777>
2025-04-22 19:53:27 +0200

    I added serach page

--- frontend/src/components/customer/searchPage/SearchPage.jsx
+++ frontend/src/components/customer/searchPage/SearchPage.jsx
@@ -1,7 +1,27 @@
 import styles from "./SearchPage.module.css";
+import React, { useState } from 'react';
 
-function SearchPage() {
-  return <h1>SearchPage</h1>;
+
+const SearchPage = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.header}>Znajdź specjalistę</h1>
+      
+      <form className={styles.searchForm}>
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Co potrzebujesz naprawić?"
+          className={styles.searchInput}
+        />
+        <button type="submit" className={styles.searchButton}>
+          Szukaj
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default SearchPage;
